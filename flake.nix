1b74f5f1392bd86c2d22b12ddd016fa0c2fe07e9
Brian Haven Kejlberg <bkc@orbit.online>
2026-04-29 20:16:30 +0200

    Maintenance: use direnv + nix flake to setup node environemt / dev shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "npm:@zeroconf/libsql";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodejs = pkgs.nodejs_24;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            nodejs
+            pkgs.typescript-language-server
+          ];
+          shellHook = ''
+            echo "node `${nodejs}/bin/node --version`"
+          '';
+        };
+        packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      }
+    );
+}
